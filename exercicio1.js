dfefd51f3776f76a20b57b3e7c12ca4488a79767
Patrick Vieira <patrick.emancipa@gmail.com>
2024-01-24 09:25:22 -0300

    Resolve o exercício 2

---
+++ exercicio1.js
@@ -0,0 +1,9 @@
+const num1 = 8;
+const num2 = 6;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
---
+++ exercicio2.js
@@ -0,0 +1,11 @@
+const num1 = 12;
+const num2 = 5;
+
+if (num1 > num2) {
+    console.log(num1);
+} else if (num1 < num2) {
+    console.log(num2);
+} else {
+    console.log('Números iguais');
+}
+
