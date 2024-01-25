c3a44128e86f31187f26d2256c71f6921b47705f
Patrick Vieira <patrick.emancipa@gmail.com>
2024-01-25 08:56:15 -0300

    Começando fundamentos 3.2

---
+++ fundamentos-secao3-dia2/exer-for.js
@@ -0,0 +1,9 @@
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    const listElements = groceryList[index];
+    console.log(listElements);
+}
+
+
+
