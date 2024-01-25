3d18aa559dc7cd6ca38156b009595a2f6e013948
Patrick Vieira <patrick.emancipa@gmail.com>
2024-01-25 08:26:25 -0300

    Termina os exercícios de fundamentos 3.1

---
+++ execicio6.js
@@ -0,0 +1,14 @@
+const num1 = 4;
+const num2 = 5;
+const num3 = 9;
+
+let even = false;
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0)) {
+    even = true;
+}
+
+console.log(even);
+
+
+
