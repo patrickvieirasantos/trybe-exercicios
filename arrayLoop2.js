c79179d3b2946b8bf0b705315e00740016346e32
Patrick Vieira <patrick.emancipa@gmail.com>
2024-01-25 09:43:20 -0300

    Começa a realizar exercícios de Fundamentos 3.2

---
+++ arrayLoop2.js
@@ -0,0 +1,19 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+};
+
+let average = sum / numbers.length
+
+let biggerThan20 = average > 20 ? 'Yes' : 'No';
+
+console.log(sum);
+console.log(average);
+console.log(biggerThan20);
+
+
+
+
+
