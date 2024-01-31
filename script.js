f61e8bc82fe57a7ea3601da4724d82f5c3027213
Patrick Vieira <patrick.emancipa@gmail.com>
2024-01-31 09:39:24 -0300

    Resolução de exercícios

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const title = document.getElementById('page-title');
+
+    title.innerText = 'Avatar';
+
+//
+
+const firstParagraph = document.getElementById('first-paragraph');
+
+firstParagraph.innerText = 'Um militar paraplégico despachado para a lua Pandora em uma missão única fica dividido entre seguir suas ordens e proteger o mundo que ele sente ser sua casa.';
+
+  //
+
+const subtitle = document.getElementById('subtitle');
+
+subtitle.innerText = 'Principais informações';
+
+//
+
+const paragraphs = document.getElementsByClassName('paragraph-style');
+
+ 
+const firstParagraph = paragraphs[0];
+firstParagraph.style.fontStyle = 'italic';
+
+//
+
+const secondParagraph = document.getElementById('second-paragraph');
+
+secondParagraph.innerText = 'Direção: James Cameron. Roteirista: James Cameron';
+
+//
+
+const subtitle = document.getElementsByTagName('h2');
+
+subtitle[0].style.color = 'grey';
+
