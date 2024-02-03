befb40bff84a374fb42f40d99714a31461e38319
Patrick Vieira <patrick.emancipa@gmail.com>
2024-02-03 11:14:50 -0300

    Faz alguns exercícios de fundamentos/4.2

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-const reader = {
-    name: 'Julia',
-    lastName: 'Pessoa',
-    age: 21,
-    favoriteBooks: [
-      {
-        title: 'O Senhor dos Anéis - a Sociedade do Anel',
-        author: 'J. R. R. Tolkien',
-        publisher: 'Martins Fontes',
-      },
-    ],
-  };
+ let whereAreYou = document.getElementById('where-are-you');
 
-  const readerName = reader.name;
-  const readerLastName = reader.lastName;
-  const titleFavoriteBook = reader.favoriteBooks.title;
-  const askedMessage = `O livro favorito de ${readerName} ${readerLastName} é ${titleFavoriteBook}`;
+let parent = whereAreYou.parentElement;
 
-  console.log(askedMessage);
+parent.style.color = 'grey';
 
-  reader.favoriteBooks.push({
-    title: 'Harry Potter e o Prisioneiro de Azkaban',
-    author: 'JK Rowling',
-    publisher: 'Rocco',
-  })
+let firstChild = parent.firstElementChild;
 
-  
-const numFavoriteBooks = reader.favoriteBooks.length;
+let firstChild2 = whereAreYou.previousElementSibling;
 
-numFavoriteBooks > 1 
-  ? console.log(`${reader.name} tem ${numFavoriteBooks} livros favoritos.`) 
-  : console.log(`${reader.name} tem ${numFavoriteBooks} livro favorito.`);
-  
+let text = whereAreYou.nextSibling;
+
+let thirdChild = whereAreYou.nextElementSibling;
+
+let thirdChild2 = parent.lastElementChild.previousElementSibling;
